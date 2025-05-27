38cce5a152f771f95b78e55d4b90ef94d668ce47
Ubuntu <ubuntu@ip-172-31-8-14.ap-south-1.compute.internal>
2025-05-27 11:19:11 +0000

    added vtricks

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from AWS shivanand');
+  res.send('Hello from AWS vtricks');
 });
 
 app.listen(PORT, () => {
